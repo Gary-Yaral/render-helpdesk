389ac3578025772998032c73dba70a4fe66c3724
Gary Yaral <gary.yaral@gmail.com>
2024-04-03 10:57:45 -0500

    First commit2

---
+++ src/app/utilities/constants.ts
@@ -0,0 +1,28 @@
+export const FORM_ACTIONS = {
+  ADD: 'Nuevo',
+  UPDATE: 'Editar'
+}
+
+// Estado de los paquetes, debe coincidir con la base de datos
+export const PACKAGE_STATUS = {
+  ENABLED: 1,
+  DISABLED: 2
+}
+
+export const SERVER = 'https://render-test-3ut5.onrender.com/api'
+
+export const ROUTES_API = {
+  auth: SERVER + '/auth/',
+  users: SERVER + '/users/',
+  statuses: SERVER + '/statuses/',
+  providers: SERVER + '/providers/',
+  invoices: SERVER + '/invoices/',
+  categories: SERVER + '/categories/',
+  inventory: SERVER + '/inventory/',
+  images: SERVER + '/uploads/',
+  refreshToken: () => ROUTES_API.auth + 'refresh-token'
+}
+
+export const SYSTEM_NAME = 'HELP DESK'
+
+export const validExtensions = ['jpg', 'jpeg', 'png']
